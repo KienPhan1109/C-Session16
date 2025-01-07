0ea2dd1767242fd8ffb7be361dad8e2b238d2140
KienPhan1109 <phantrungkien2006@gmail.com>
2025-01-07 16:35:49 +0700

    Update Session16-04.c

--- Session16-04.c
+++ Session16-04.c
@@ -1,17 +1,15 @@
 #include <stdio.h>
 
-int array(int *a, int size)
-{
-    for (int i = 0; i < size; i++)
-    {
-        printf("%d ", a[i]);
+void printArray(int *arr, int size) {
+    for (int i = 0; i < size; i++) {
+        printf("%d ", *(arr + i));
     }
+    printf("\n");
 }
 
-int main()
-{
+int main() {
     int a[] = {1, 2, 3, 4, 5};
     int size = sizeof(a) / sizeof(a[0]);
-    array(a, size); 
+    printArray(a, size);
     return 0;
 }
