b0e5efbed527a9a28e04f93808fecd392c6c8de1
KienPhan1109 <phantrungkien2006@gmail.com>
2025-01-07 16:02:40 +0700

    Update Session16-02.c

--- Session16-02.c
+++ Session16-02.c
@@ -1,18 +1,20 @@
 #include <stdio.h>
 
-int updataUpdate(int a[], int size, int value, int pos)
-{
-    for (int i = 0; i < size; i++)
-    {
-        if (a[i] == pos)
-        {
-            a[i] = value;
-            return 1;
-        }
-    }
-    
+void swap(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
 }
 
-int main(){
-    
+int main() {
+    int x, y;
+    printf("Nhập vào x: ");
+    scanf("%d", &x);
+    printf("Nhập vào y: ");
+    scanf("%d", &y);
+    printf("Trước khi đổi giá trị:\n");
+    printf("x = %d, y = %d\n", x, y);
+    swap(&x, &y);
+    printf("Sau khi đổi giá trị:\n");
+    printf("x = %d, y = %d\n", x, y);
 }
